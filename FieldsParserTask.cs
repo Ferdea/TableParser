b61c7f496f8916082fa77c1dbf61112d69aca5f3
Саша Сапегин <sapegin04@mail.ru>
2022-10-23 01:06:05 +0500

    Add FieldParser

--- FieldsParserTask.cs
+++ FieldsParserTask.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Text;
 using NUnit.Framework;
 
 namespace TableParser
@@ -16,24 +17,80 @@ namespace TableParser
             }
         }
 
-        // Скопируйте сюда метод с тестами из предыдущей задачи.
+        [TestCase("text", new[] {"text"})]
+        [TestCase("hello world", new[] {"hello", "world"})]
+        [TestCase("\"hello world\"", new[] {"hello world"})]
+        [TestCase("text    space", new[] {"text", "space"})]
+        [TestCase("\"hello \\\"world\"", new[] {"hello \"world"})]
+        [TestCase("\"hello \\\\world\"", new[] {"hello \\world"})]
+        [TestCase("\"hello 'world\"", new[] {"hello 'world"})]
+        [TestCase("\"hello \\'world\"", new[] {"hello 'world"})]
+        [TestCase("hello world", new[] {"hello", "world"})]
+        [TestCase("'hello world'", new[] {"hello world"})]
+        [TestCase("'hello \\\"world'", new[] {"hello \"world"})]
+        [TestCase("'hello \\\\world'", new[] {"hello \\world"})]
+        [TestCase("'hello \\'world'", new[] {"hello 'world"})]
+        [TestCase("'hello \\'world'", new[] {"hello 'world"})]
+        [TestCase("'hello \\'world'", new[] {"hello 'world"})]
+        [TestCase("text \"rlh\"", new[] {"text", "rlh"})]
+        [TestCase("\"rlh\" text", new[] {"rlh", "text"})]
+        [TestCase("\"hello world", new[] {"hello world"})]
+        [TestCase("text\"rlh\"", new[] {"text", "rlh"})]
+        [TestCase("\"hello world ", new[] {"hello world "})]
+        [TestCase(" text", new[] {"text"})]
+        [TestCase("", new string[] {})]
+        [TestCase("''", new[] {""})]
+        [TestCase("'\\\\'", new[] {"\\"})]
+        public static void RunTests(string input, string[] expectedOutput)
+        {
+            Test(input, expectedOutput);
+        }
     }
 
     public class FieldsParserTask
     {
-        // При решении этой задаче постарайтесь избежать создания методов, длиннее 10 строк.
-        // Подумайте как можно использовать ReadQuotedField и Token в этой задаче.
         public static List<Token> ParseLine(string line)
         {
-            return new List<Token> { ReadQuotedField(line, 0) }; // сокращенный синтаксис для инициализации коллекции.
+            var result = new List<Token>();
+            var i = 0;
+            while (i < line.Length)
+            {
+                if (line[i] == ' ')
+                {
+                    i++;
+                    continue;
+                }
+                
+                if (line[i] == '\'' || line[i] == '\"')
+                {
+                    result.Add(ReadQuotedField(line, i));
+                }
+                else
+                {
+                    result.Add(ReadField(line, i));
+                }
+
+                i += result[result.Count - 1].Length;
+            }
+
+            return result;
         }
         
         private static Token ReadField(string line, int startIndex)
         {
-            return new Token(line, 0, line.Length);
+            var field = new StringBuilder();
+            var length = 0;
+            for (var i = startIndex; i < line.Length; i++)
+            {
+                if (line[i] == '\"' || line[i] == '\'' || line[i] == ' ')
+                    break;
+                length++;
+                field.Append(line[i]);
+            }
+            return new Token(field.ToString(), startIndex, length);
         }
 
-        public static Token ReadQuotedField(string line, int startIndex)
+        private static Token ReadQuotedField(string line, int startIndex)
         {
             return QuotedFieldTask.ReadQuotedField(line, startIndex);
         }
